e892ba66408880425ffd5a9c3586161d0a8345e7
SridharVadla <vadlasreedhar123@gmail.com>
2024-06-25 16:47:54 +0530

    Initialized an Spring Boot Application for Cards Microservice

---
+++ src/test/java/com/Bankify/Cards/CardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Bankify.Cards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
